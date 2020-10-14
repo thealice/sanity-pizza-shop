fe75f6287de3be06847ab39c0cf55151f0b03a08
Alice Freda <alicefreda@gmail.com>
2020-10-14 11:58:17 -0700

    create pizza schema

---
+++ schemas/pizza.js
@@ -0,0 +1,41 @@
+import { MdLocalPIzza as icon } from 'react-icons/md';
+
+export default {
+  name: 'pizza',
+  title: 'Pizzas',
+  type: 'document',
+  icon,
+  fields: [
+    {
+      name: 'name',
+      title: 'Pizza Name',
+      type: 'string',
+      description: 'The name of the pizza',
+    },
+    {
+      name: 'slug',
+      title: 'Slug',
+      type: 'slug',
+      options: {
+        source: 'name',
+        maxLength: 100,
+      },
+    },
+    {
+      name: 'image',
+      title: 'Image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    },
+    {
+      name: 'price',
+      title: 'Price',
+      type: 'number',
+      description: 'Price of the pizza in cents',
+      validation: (Rule) => Rule.min(1000).max(50000),
+      // TODO: add custom input component
+    },
+  ],
+};
