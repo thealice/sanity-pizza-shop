61c407eb9aa7cf73c08579ed832b4131af8108c0
Alice Freda <alicefreda@gmail.com>
2020-10-15 06:28:54 -0700

    add preview to pizza schema

--- schemas/pizza.js
+++ schemas/pizza.js
@@ -44,4 +44,14 @@ export default {
       of: [{ type: 'reference', to: [{ type: 'topping' }] }],
     },
   ],
+  preview: {
+    select: {
+      title: 'name',
+      media: 'image',
+      topping0: 'toppings.0.name',
+      topping1: 'toppings.1.name',
+      topping2: 'toppings.2.name',
+      topping3: 'toppings.3.name',
+    },
+  },
 };
